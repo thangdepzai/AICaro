1b697018cd6bbd17372f6e5f5f0dfef3af8541fd
DESKTOP-Q3VI6FE\thang <tabaothang97@gmail.com>
2018-05-03 10:16:03 +0700

    done Anpla beta

---
+++ app/src/main/java/vn/edu/hust/thangtb/aicaro/AsyncTaskGame.java
@@ -0,0 +1,12 @@
+package vn.edu.hust.thangtb.aicaro;
+
+/**
+ * Created by thang on 3/17/2018.
+ */
+
+public class AsyncTask extends android.os.AsyncTask<String, String, String> {
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
---
+++ app/src/main/java/vn/edu/hust/thangtb/aicaro/State.java
@@ -0,0 +1,8 @@
+package vn.edu.hust.thangtb.aicaro;
+
+/**
+ * Created by thang on 3/15/2018.
+ */
+
+public class State {
+}
